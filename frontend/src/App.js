32474ff7b0f5c18372a41ee81816766fc3844532
nitkkrjatin <jk.aggarwal748@gmail.com>
2022-11-06 11:55:03 +0530

    done

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import TextField from '@mui/material/TextField'
+function App() {
+  return (
+    <div className='centered-div'>
+      <TextField label='Outlined' id='outlined-basic' />
+    </div>
+  )
+}
+
+export default App
